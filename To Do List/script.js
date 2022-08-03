74c27c5f58fc773b8387ba206e37edab4124a039
Jason <jasonleostudios@gmail.com>
2022-08-03 16:17:51 -0400

    Local Storage & List Creation

--- To Do List/script.js
+++ To Do List/script.js
@@ -1,9 +1,22 @@
+// 17:00
+
 
 const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]');
 
-let lists = [];
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+
+listsContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId;
+        saveAndRender();
+    }
+})
 
 newListForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -16,11 +29,20 @@ newListForm.addEventListener('submit', e => {
     newListInput.value = null;
 
     lists.push(list);
-    render();
+    saveAndRender();
 })
 
 function createList(name) {
     return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function saveAndRender() {
+    save();
+    render();
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
 }
 
 function render() {
@@ -32,6 +54,8 @@ function render() {
         listElement.dataset.listId = list.id;
         listElement.classList.add('list');
         listElement.innerText = list.name;
+
+        if (list.id === selectedListId) listElement.classList.add('selected-list')
 
         listsContainer.appendChild(listElement);
     })
